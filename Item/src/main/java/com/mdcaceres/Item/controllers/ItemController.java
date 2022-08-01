7d9c967939b21dbb4c382c818f913e634d0d0471
mdcaceres <mdcaceres95@gmail.com>
2022-08-01 17:47:26 -0300

    config server, entornos, actuator resfresScope

--- Item/src/main/java/com/mdcaceres/Item/controllers/ItemController.java
+++ Item/src/main/java/com/mdcaceres/Item/controllers/ItemController.java
@@ -8,15 +8,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+//refresscope para actualizar (componentes spring, controllers) de forma automatica sin reiniciar el servidor
+@RefreshScope
 @RestController
 public class ItemController {
 
     private final Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private CircuitBreakerFactory circuitFactory;
@@ -24,6 +36,10 @@ public class ItemController {
     //@Qualifier("serviceFeign")
     @Qualifier("serviceRestTemplate")
     private ItemService itemService;
+
+    //@Value("${configuracion.texto}")
+    //private String texto;
+
 
     @GetMapping("/listar")
     public List<Item> listar(@RequestParam(name = "nombre", required = false) String nombre,
@@ -36,6 +52,19 @@ public class ItemController {
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
         return circuitFactory.create("items")
                 .run(() -> itemService.findById(id,cantidad), e -> metodoAlternativo(id,cantidad, e));
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<Map<String,String>> obtenerConfig(@Value("${server.port}") String puerto,@Value("${configuracion.texto}") String texto){
+        log.info("puerto de items es " + texto);
+        Map<String,String> json = new HashMap<>();
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+        if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            json.put("autor", env.getProperty("configaracion.autor.nombre"));
+            json.put("email", env.getProperty("configaracion.autor.email"));
+        }
+        return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
     public Item metodoAlternativo(Long id, Integer cantidad, Throwable e){
